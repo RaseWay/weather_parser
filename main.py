8ac6baa1282e3b4d9cb72ceb7b8b5d554e2d2812
ruslan <raseway@mail.ru>
2026-06-12 21:44:35 +0500

    Created class CityWeather

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello, Weather Parser")
+class CityWeather:
+    def __init__(self, name: str, temperature: float, condition: str, wind_speed: float):
+        self.name = name
+        self.temperature = temperature
+        self.condition = condition
+        self.wind_speed = wind_speed
+
+    def __str__(self):
+        return f"В городе {self.name} сейчас {self.temperature}*C, на улице: {self.condition}. Скорость ветра на улице: {self.wind_speed}"
