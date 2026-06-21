7735e2c7bb4359aacb3816c5d9b2c4a634c70bba
ruslan <raseway@mail.ru>
2026-06-21 15:41:08 +0500

    main.py update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import requests
+
 class CityWeather:
     def __init__(self, name: str, temperature: float, condition: str, wind_speed: float):
         self.name = name
@@ -6,4 +8,26 @@ class CityWeather:
         self.wind_speed = wind_speed
 
     def __str__(self):
-        return f"В городе {self.name} сейчас {self.temperature}*C, на улице: {self.condition}. Скорость ветра на улице: {self.wind_speed}"
+        return f"В городе {self.name} сейчас {self.temperature}*C, на улице: {self.condition}. Скорость ветра на улице: {self.wind_speed} метра в секунду"
+
+API_KEY = "f1b99a75e2a74a0882690228262106"
+CITY = "Tyumen"
+
+url = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={CITY}&lang=ru"
+
+response = requests.get(url)
+data = response.json()
+
+city_name = data["location"]["name"]
+temperature = data["current"]["temp_c"]
+condition = data["current"]["condition"]["text"]
+wind_speed_ms = round(data["current"]["wind_kph"]/3.6, 1)
+
+current_weather = CityWeather(
+    name=city_name,
+    temperature=temperature,
+    condition=condition,
+    wind_speed=wind_speed_ms
+)
+
+print(current_weather)
